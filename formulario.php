ee7023eaa278b73d9d548992a91bd31a6f1552ac
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-17 07:40:42 -0400

    tela de login

--- formulario.php
+++ formulario.php
@@ -22,7 +22,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Formulario</title>
     <style>
         body {
             font-family: Arial, Helvetica, sans-serif;
