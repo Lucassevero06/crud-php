26a18442b665c6db03eb3a80bbce59ffa428813a
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-17 18:36:50 -0400

    melhorando estilização da pagina de login

--- login.php
+++ login.php
@@ -7,18 +7,29 @@
     <style>
         body {
             font-family: Arial, Helvetica, sans-serif;
-            background-image: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+            background: rgb(0,255,207);
+            background: linear-gradient(243deg, rgba(0,255,207,1) 20%, rgba(20,255,0,1) 100%);
+        }
+        
+        .box {
+            height: 100vh;
+            width: 100%;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
         }
 
-        div {
-            background-color: rgba(0, 0, 0, 0.6);
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            padding: 80px;
+        .boxForm {
+            background-color: white;
+            padding: 30px;
             border-radius: 15px;
-            color: white;
+            width: 400px;
+            text-align: center;
+        }
+
+        .boxImg {
+            width: 50%;
+            border-right: 1px solid white;
         }
 
         input {
@@ -26,37 +37,43 @@
             border: none;
             outline: none;
             font-size: 15px;
+            width: 90%;
         }
 
         .inputSubmit {
-            background-color: dodgerblue;
+            background-color: rgba(20,255,0,1);
             border: none;
             padding: 15px;
             width: 100%;
             border-radius: 10px;
             color: white;
+            font-weight: bold;
             font-size: 15px;
         }
 
         .inputSubmit:hover {
-            background-color: deepskyblue;
+            background-color: rgba(0,255,207,1);
             cursor: pointer;
         }
     </style>
 </head>
 <body>
-    <a href="home.php">Voltar</a>
-    <div>
-        <h1>Login</h1>
-        <form action="testLogin.php" method="post">
-            <input type="text" placeholder="Nome" name="email">
-            <br>
-            <br>
-            <input type="password" placeholder="Senha" name="senha">
-            <br>
-            <br>
-            <input class="inputSubmit" type="submit" name="submit" value="Enviar">
-        </form>
+    <div class="box">
+        <div class="boxImg">
+            <img src="./undraw_mobile_user_re_xta4.svg" alt="Foto Login">
+        </div>
+        <div class="boxForm">
+            <h1>Login</h1>
+            <form action="testLogin.php" method="post">
+                <input type="text" placeholder="Nome" name="email">
+                <br>
+                <br>
+                <input type="password" placeholder="Senha" name="senha">
+                <br>
+                <br>
+                <input class="inputSubmit" type="submit" name="submit" value="Enviar">
+            </form>
+        </div>
     </div>
 </body>
 </html>
